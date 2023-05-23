d696b2cc5503f7ab1d90371ed68b92c976533a47
aliccciao <alicia.utomo@gmail.com>
2023-05-23 20:55:00 +0700

    rename & delete folders

--- landing_page/apps.py
+++ landing_page/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NavbarConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kira'
+    name = 'landing_page'
--- registration/urls.py
+++ registration/urls.py
@@ -6,6 +6,6 @@ app_name = 'registration'
 urlpatterns = [
     path('', show_registration, name='reg'),
     path('atlet', show_registration_atlet, name='reg_atlet'),
-    path('pelatih', show_registration_pelatih, name='dash_pelatih'),
+    path('pelatih', show_registration_pelatih, name='reg_pelatih'),
     path('umpire', show_registration_umpire, name='reg_umpire'),
 ]
