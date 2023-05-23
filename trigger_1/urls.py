c2afea330374ba4f1d35589759dd09042087f629
Monica Oktaviona <monica.oktaviona@ui.ac.id>
2023-05-23 23:02:17 +0700

    add logout urls and fix logout link in navbar

--- trigger_1/urls.py
+++ trigger_1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from trigger_1.views import *
 
-app_name = 'trigger_!'
+app_name = 'trigger_1'
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
