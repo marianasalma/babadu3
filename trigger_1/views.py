b05ca7c1dfba4b724a09b2762e8da431851571ad
Monica Oktaviona <monica.oktaviona@ui.ac.id>
2023-05-23 22:48:35 +0700

    change sessions & add name & email validator

--- trigger_1/views.py
+++ trigger_1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from collections import namedtuple
 from django.db import connection
+from django.contrib import messages
 
 
 def namedtuplefetchall(cursor):
@@ -27,23 +28,13 @@ def get_query(str):
 def is_authenticated(request):
     '''Check if user in a session'''
     try:
-        request.session["email"]
+        request.session["id"]
         return True
     except KeyError:
         return False
 
 
 def get_role(nama, email):
-    # railway
-    # atlet_query = get_query(
-    #     f'''
-    #     SELECT nama, email
-    #     FROM member INNER JOIN atlet ON member.id=atlet.id
-    #     WHERE nama='{nama}' AND email='{email}';
-    #     '''
-    # )
-
-    # lokal
     atlet_query = get_query(
         f'''
         SELECT nama, email
@@ -51,48 +42,105 @@ def get_role(nama, email):
         WHERE nama='{nama}' AND email='{email}';
         '''
     )
+
+    umpire_query = get_query(
+        f'''
+        SELECT nama, email
+        FROM member as m INNER JOIN umpire as u ON m.id=u.id
+        WHERE nama='{nama}' AND email='{email}';
+        '''
+    )
+
+    pelatih_query = get_query(
+        f'''
+        SELECT nama, email
+        FROM member as m INNER JOIN pelatih as p ON m.id=p.id
+        WHERE nama='{nama}' AND email='{email}';
+        '''
+    )
+
     if type(atlet_query) == list and len(atlet_query) != 0:
-        print("atlet")
+        print("atlet")      #to be deleted
         return "atlet"
+    elif type(umpire_query) == list and len(umpire_query) != 0:
+        print("umpire")     #to be deleted
+        return "umpire"
+    elif type(pelatih_query) == list and len(pelatih_query) != 0:
+        print("pelatih")    #to be deleted
+        return "pelatih"
+    
+def get_id(nama, email):
+    print("get id")
+    query = get_query(
+        f'''
+        SELECT id
+        FROM member
+        WHERE nama='{nama}' AND email='{email}';
+        '''
+    )[0]
+    return str(query.id)
+
 
 
 # Create your views here.
 def login(request):
     request.session.flush()
     request.session.clear_expired()
-    print("here")
+    print("login")      #to be deleted
+
     if request.method != "POST" and not is_authenticated(request):
-        print("if1")
-        print(request.method)
         return render(request, 'login.html')
 
     if is_authenticated(request):
-        print("if 2")
-        nama = str(request.session["nama"])
-        email = str(request.session["email"])
+        print("is authenticated")       #to be deleted
+        id = str(request.session["id"])
+
+        # to be deleted
+        print('Type', type(request.session["id"]), 'UIID', request.session["id"]) 
+
     else:
-        print("else")
+        print("is not authenticated")       #to be deleted
         nama = str(request.POST["nama"])
         email = str(request.POST["email"])
 
-    print("here2")
-    role = get_role(nama, email)
-    print("role")
-    request.session["nama"] = nama
-    request.session["email"] = email
-    request.session["role"] = role
-    request.session.set_expiry(0)
-    request.session.modified = True
+    try:
+        role = get_role(nama, email)
+        id = get_id(nama, email)
+        request.session["id"] = id
+        request.session["role"] = role
+        request.session.set_expiry(0)
+        request.session.modified = True
+    except:
+        if not is_authenticated(request):
+            messages.error(request, 'Email atau password salah')
+            return render(request, 'login.html')
+
+    #to be deleted
     print("DATA:")
     print(nama)
     print(email)
     print(role)
+    print(id)
+    print()
+    print(request.session["id"])
+    print(request.session["role"])
+    print()
+    print("type")
+    print('Type', type(request.session["id"]), 'UIID', request.session["id"])
 
     if role == "atlet":
-        print("there")
-        return redirect("../../dash/atlet")
+        print("redirecting to atlet dashboard")     #to be deleted
+        # return redirect("../../dash/atlet")
+        return redirect("../../trigger_4/daftar-event/")
 
-    return render(request, "login.html")
+    elif role == "umpire":
+        print("redirecting to umpire dashboard")     #to be deleted
+        return redirect("../../dash/umpire")
+    elif role == "pelatih":
+        print("redirecting to pelatih dashboard")     #to be deleted
+        return redirect("../../dash/pelatih")
+
+    return render(request, "login.html", messages)
 
 
 def logout(request):
